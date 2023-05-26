96e00f3909d443a52b2703c58dfdde2556be5196
Mason Remaley <mason@anthropicstudios.com>
2023-05-26 03:13:44 -0700

    Allows whole number floats as ints

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const Ast = std.zig.Ast;
 const NodeIndex = std.zig.Ast.Node.Index;
 const Type = std.builtin.Type;
 const Base = std.zig.number_literal.Base;
+const FloatBase = std.zig.number_literal.FloatBase;
 const assert = std.debug.assert;
 
 const Parser = @This();
@@ -54,6 +55,7 @@ fn parseSignedNumber(self: *const Parser, comptime T: type, node: NodeIndex, sig
             switch (number) {
                 .int => |int| return applySignToInt(T, sign, int),
                 .big_int => |base| return self.parseBigInt(T, sign, node, base),
+                .float => |float_base| return self.parseFloat(T, sign, node, float_base),
                 else => unreachable,
             }
         },
@@ -96,7 +98,6 @@ fn applySignToInt(comptime T: type, sign: Sign, value: anytype) T {
     }
 }
 
-// XXX: don't generate this except for cases that will actually need it?
 fn parseBigInt(self: *const Parser, comptime T: type, sign: Sign, node: NodeIndex, base: Base) T {
     const data = self.ast.nodes.items(.data);
     const bytes_node = switch (sign) {
@@ -116,6 +117,89 @@ fn parseBigInt(self: *const Parser, comptime T: type, sign: Sign, node: NodeInde
         }
     }
     return result;
+}
+
+fn parseFloat(
+    self: *const Parser,
+    comptime T: type,
+    sign: Sign,
+    node: NodeIndex,
+    base: FloatBase,
+) T {
+    const F = switch (@typeInfo(T)) {
+        .Float => T,
+        .Int => f128,
+        else => unreachable,
+    };
+    _ = base; // XXX: ???
+    const data = self.ast.nodes.items(.data);
+    const bytes_node = switch (sign) {
+        .positive => data[node].lhs,
+        .negative => node,
+    };
+    const bytes = self.ast.tokenSlice(bytes_node);
+    const unsigned_float = std.fmt.parseFloat(F, bytes) catch unreachable;
+    const result = switch (sign) {
+        .negative => -unsigned_float,
+        .positive => unsigned_float,
+    };
+    if (T == F) {
+        return result;
+    } else {
+        return floatToInt(T, result).?;
+    }
+}
+
+fn floatToInt(comptime T: type, value: anytype) ?T {
+    switch (@typeInfo(@TypeOf(value))) {
+        .Float, .ComptimeFloat => {},
+        else => @compileError(@typeName(@TypeOf(value)) ++ " is not a floating point type"),
+    }
+    switch (@typeInfo(T)) {
+        .Int, .ComptimeInt => {},
+        else => @compileError(@typeName(T) ++ " is not an integer type"),
+    }
+
+    if (T == comptime_int or (value > std.math.maxInt(T) or value < std.math.minInt(T))) {
+        return null;
+    }
+
+    if (std.math.isNan(value) or std.math.trunc(value) != value) {
+        return null;
+    }
+
+    return @floatToInt(T, value);
+}
+
+test "floatToInt" {
+    // Valid conversions
+    try std.testing.expectEqual(@as(u8, 10), floatToInt(u8, 10.0).?);
+    try std.testing.expectEqual(@as(i8, -123), floatToInt(i8, @as(f64, -123.0)).?);
+    try std.testing.expectEqual(@as(i16, 45), floatToInt(i16, @as(f128, 45.0)).?);
+    try std.testing.expectEqual(@as(comptime_int, 10), comptime floatToInt(i16, @as(f32, 10.0)).?);
+    try std.testing.expectEqual(
+        @as(comptime_int, 10),
+        comptime floatToInt(i16, @as(comptime_float, 10.0)).?,
+    );
+    try std.testing.expectEqual(@as(u8, 5), floatToInt(u8, @as(comptime_float, 5.0)).?);
+
+    // Out of range
+    try std.testing.expectEqual(@as(?u4, null), floatToInt(u4, @as(f32, 16.0)));
+    try std.testing.expectEqual(@as(?i4, null), floatToInt(i4, -17.0));
+    try std.testing.expectEqual(@as(?u8, null), floatToInt(u8, -2.0));
+
+    // Not a whole number
+    try std.testing.expectEqual(@as(?u8, null), floatToInt(u8, 0.5));
+    try std.testing.expectEqual(@as(?i8, null), floatToInt(i8, 0.01));
+
+    // Infinity and NaN
+    try std.testing.expectEqual(@as(?u8, null), floatToInt(u8, std.math.inf(f32)));
+    try std.testing.expectEqual(@as(?u8, null), floatToInt(u8, -std.math.inf(f32)));
+    try std.testing.expectEqual(@as(?u8, null), floatToInt(u8, std.math.nan(f32)));
+    try std.testing.expectEqual(
+        @as(?comptime_int, null),
+        comptime floatToInt(comptime_int, std.math.inf(f32)),
+    );
 }
 
 test "parseInt" {
@@ -155,7 +239,7 @@ test "parseInt" {
         try parse(allocator, i65, "-18446744073709551616"),
     );
 
-    // XXX: ...
-    // try std.testing.expectEqual(parse(allocator, i8, "-1.0"), -1);
-    // XXX: is char allowed for non u8s?
+    // Test parsing whole number floats as integers
+    try std.testing.expectEqual(@as(i8, -1), try parse(allocator, i8, "-1.0"));
+    try std.testing.expectEqual(@as(i8, 123), try parse(allocator, i8, "123.0"));
 }
