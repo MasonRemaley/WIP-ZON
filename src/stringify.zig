1385a2f30ec169bacaae3751839ffb0b0fd1dda0
Mason Remaley <mason@anthropicstudios.com>
2023-07-06 20:07:36 -0700

    Stringifies (and escapes!) strings

--- src/stringify.zig
+++ src/stringify.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 
 pub const Options = struct {
-    // XXX: clearer api?
+    // XXX: clearer api? defaults?
     indent_level: ?usize = 0,
+    strings: bool = true,
 
     pub fn outputIndent(self: @This(), out_stream: anytype) @TypeOf(out_stream).Error!void {
         if (self.indent_level) |indent_level| {
@@ -52,23 +53,24 @@ pub fn stringify(value: anytype, options: Options, out_stream: anytype) !void {
             try out_stream.writeByte('.');
             try out_stream.writeAll(@tagName(value));
         },
-        .Array => {
-            var child_options = options;
-            try out_stream.writeAll(".{");
-            if (value.len > 0) {
-                child_options.indent();
-                for (value, 0..) |item, i| {
-                    try child_options.outputIndent(out_stream);
-                    try stringify(item, child_options, out_stream);
-                    if (child_options.indent_level != null or i != value.len - 1) {
-                        try out_stream.writeByte(',');
-                    }
-                }
-                try child_options.unindent(out_stream);
+        .Array => try stringifyList(value, options, out_stream),
+        .Pointer => |pointer| {
+            const is_string = switch (pointer.size) {
+                .One => switch (@typeInfo(pointer.child)) {
+                    .Array => |array| array.child == u8,
+                    else => failToStringifyType(@TypeOf(value)),
+                },
+                .Many, .Slice, .C => pointer.child == u8,
+            };
+
+            // Format as either a list or a string.
+            if (is_string and options.strings) {
+                try out_stream.print("\"{}\"", .{std.zig.fmtEscapes(value)});
+            } else {
+                try out_stream.writeByte('&');
+                try stringifyList(value, options, out_stream);
             }
-            try out_stream.writeByte('}');
         },
-        // .Pointer => self.stringifyPointer(options, value, out_stream),
         // .Struct => |Struct| if (Struct.is_tuple)
         //     self.stringifyTuple(options, value, out_stream)
         // else
@@ -77,6 +79,23 @@ pub fn stringify(value: anytype, options: Options, out_stream: anytype) !void {
 
         else => failToStringifyType(@TypeOf(value)),
     }
+}
+
+fn stringifyList(value: anytype, options: Options, out_stream: anytype) !void {
+    var child_options = options;
+    try out_stream.writeAll(".{");
+    if (value.len > 0) {
+        child_options.indent();
+        for (value, 0..) |item, i| {
+            try child_options.outputIndent(out_stream);
+            try stringify(item, child_options, out_stream);
+            if (child_options.indent_level != null or i != value.len - 1) {
+                try out_stream.writeByte(',');
+            }
+        }
+        try child_options.unindent(out_stream);
+    }
+    try out_stream.writeByte('}');
 }
 
 fn failToStringifyType(comptime T: type) noreturn {
@@ -99,6 +118,7 @@ test "stringify basic" {
     const Exhaustive = enum { A, B };
     const NonExhaustive = enum(u8) { A, B, _ };
 
+    // Primitives
     try expectStringifyEqual(true, .{}, "true");
     try expectStringifyEqual(false, .{}, "false");
     try expectStringifyEqual(@as(u32, 123), .{}, "123");
@@ -111,9 +131,21 @@ test "stringify basic" {
     try expectStringifyEqual(NonExhaustive.B, .{}, ".B");
     try expectStringifyEqual(@as(NonExhaustive, @enumFromInt(3)), .{}, "@enumFromInt(3)");
     try expectStringifyEqual(.abc, .{}, ".abc");
-    try expectStringifyEqual([_]u8{ 1, 2, 3 }, .{}, ".{\n    1,\n    2,\n    3,\n}");
-    // XXX: skip trailing comma if no newlines?
-    try expectStringifyEqual([_]u8{ 1, 2, 3 }, .{ .indent_level = null }, ".{1,2,3}");
-    try expectStringifyEqual([_]u8{}, .{}, ".{}");
-    try expectStringifyEqual([_]u8{}, .{ .indent_level = null }, ".{}");
+
+    // Arrays
+    try expectStringifyEqual([_]u16{ 1, 2, 3 }, .{}, ".{\n    1,\n    2,\n    3,\n}");
+    try expectStringifyEqual([_]u16{ 1, 2, 3 }, .{ .indent_level = null }, ".{1,2,3}");
+    try expectStringifyEqual([_]u16{}, .{}, ".{}");
+    try expectStringifyEqual([_]u16{}, .{ .indent_level = null }, ".{}");
+
+    // XXX: what about sentinels and stuff, does it matter here?
+    // Slices
+    try expectStringifyEqual(@as([]const u16, &.{ 1, 2, 3 }), .{}, "&.{\n    1,\n    2,\n    3,\n}");
+    try expectStringifyEqual(@as([]const u16, &.{ 1, 2, 3 }), .{ .indent_level = null }, "&.{1,2,3}");
+    try expectStringifyEqual(@as([]const u16, &.{}), .{}, "&.{}");
+    try expectStringifyEqual(@as([]const u16, &.{}), .{ .indent_level = null }, "&.{}");
+
+    // Slices of chars
+    try expectStringifyEqual("abc", .{ .indent_level = null, .strings = false }, "&.{97,98,99}");
+    try expectStringifyEqual("ab\"c", .{ .indent_level = null }, "\"ab\\\"c\"");
 }
